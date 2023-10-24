ef8503aac4ac478c27480a6fc50cbea1b890e7c8
khuram2025 <khuram2025@gmail.com>
2023-10-24 19:30:12 +0300

    working on template

---
+++ chan/home/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+
+def product_detail(request, product_id):
+    # Dummy product data
+    product = {
+        'id': product_id,
+        'name': 'Sample Product',
+        'description': 'This is a sample product description.',
+        'price': '29.99',
+    }
+    return render(request, 'product/product_detail.html', {'product': product})
+
+def index(request):
+    return render(request, 'home/index.html')
