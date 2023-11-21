3914ddebe4f42027cd8714472119d73fa0617975
khuram2025 <khuram2025@gmail.com>
2023-11-21 08:33:23 +0300

    change static01

--- cstore/cstore/settings/prod.py
+++ cstore/cstore/settings/prod.py
@@ -21,7 +21,7 @@ DATABASES = {
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
